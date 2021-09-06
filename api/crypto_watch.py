796375b2fa6b37e098acdf47a6f584665904547a
Kevin Kim <kevinkim@Kevins-MacBook-Pro.local>
2021-09-07 05:21:45 +0900

    refactoring crypto watch api

--- api/crypto_watch.py
+++ api/crypto_watch.py
@@ -6,32 +6,12 @@ from requests import Request, Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 
 
-# 'https://api.cryptowat.ch/markets/:exchange/:pair/ohlc'
-
 BASE_URL='https://api.cryptowat.ch'
 
 EXCHANGE_ENDPOINT='/exchanges'
 MARKET_ENDPOINT='/markets'
 
 
-'''
-A stock market is a platform for trading of a company’s stocks and derivatives at an agreed price. 
-Supply and demand of shares drive the stock market. In any country stock market is one of the most emerging sectors. 
-Nowadays, many people are indirectly or directly related to this sector. 
-Therefore, it becomes essential to know about market trends. 
-Thus, with the development of the stock market, people are interested in forecasting stock price. 
-But, due to dynamic nature and liable to quick changes in stock price, prediction of the stock price becomes a challenging task. 
-Stock markets are mostly a non-parametric, non-linear, noisy and deterministic chaotic system (Ahangar et al. 2010).
-
-As the technology is increasing, stock traders are moving towards to use Intelligent Trading Systems rather than fundamental analysis for predicting prices of stocks, 
-which helps them to take immediate investment decisions. One of the main aims of a trader is to predict the stock price such that he can sell it before its value decline, 
-or buy the stock before the price rises. The efficient market hypothesis states that it is not possible to predict stock prices and that stock behaves in the random walk. 
-It seems to be very difficult to replace the professionalism of an experienced trader for predicting the stock price. 
-But because of the availability of a remarkable amount of data and technological advancements we can now formulate an appropriate algorithm for prediction 
-whose results can increase the profits for traders or investment firms. 
-
-Thus, the accuracy of an algorithm is directly proportional to gains made by using the algorithm.
-'''
 
 def get_exchanges():
     req_url = 'https://api.cryptowat.ch/exchanges'
@@ -71,10 +51,10 @@ def get_historical_data(symbol, exchange, start, end, interval):
     try:
       response = session.get(req_url, params=parameters)
       data = json.loads(response.text)
-      print(data)
-      # current_quote = data['data']['BTC']['quote']
+      historical_data = data['result'][str(interval)]
+      print(historical_data)
 
-      # return current_quote
+      return historical_data
 
     except (ConnectionError, Timeout, TooManyRedirects) as e:
       print(e)
